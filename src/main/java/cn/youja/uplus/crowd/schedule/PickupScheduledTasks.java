bb6b92fa8006b32336fa94dee36ceacbf2a6619d
Albert Sun <atschx@gmail.com>
2015-08-18 22:17:13 +0800

    new feature

--- src/main/java/cn/youja/uplus/crowd/schedule/PickupScheduledTasks.java
+++ src/main/java/cn/youja/uplus/crowd/schedule/PickupScheduledTasks.java
@@ -1,5 +1,7 @@
 package cn.youja.uplus.crowd.schedule;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,8 +15,10 @@ public class PickupScheduledTasks {
 	/**
 	 * 凌晨零点1分，任务分拣。
 	 */
-	@Scheduled(cron = "0 1 0 * * ? ")
+//	@Scheduled(cron = "0 1 0 * * ? ")
+	@Scheduled(fixedRate = 200)
 	public void splitByGender() {
 		//--
+		System.out.println("-------"+new Date());
 	}
 }
